4206bd5da90ad6b060699574bab685c9ab30fde7
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-06-21 15:56:08 +0900

    #14916

---
+++ 구현/[S5] 14916 거스름돈.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while (true) {
+            if (n % 5 == 0) {
+                cnt += n / 5;
+                break;
+            }
+            else {
+                n -= 2;
+                cnt++;
+            }
+            if (n < 0) {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
