e50b75f7e713f112a6ea7f890e2869cbb42b2ed4
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-08-14 14:47:52 +0900

    #2458
    플로이드-워셜 알고리즘
    시간복잡도 O(N^3)

---
+++ 알고리즘/Shortest Path/[G4] 2458 키 순서.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] dist;
+    static final int INF = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dist = new int[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(dist[i], INF);
+        }
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            dist[a][b] = 1;
+        }
+
+        for (int k = 1; k < N + 1; k++) {
+            for (int i = 1; i < N + 1; i++) {
+                for (int j = 1; j < N + 1; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i < N + 1; i++) {
+            int j;
+            for (j = 1; j < N + 1; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (dist[i][j] == INF && dist[j][i] == INF) {
+                    break;
+                }
+            }
+            if (j == N + 1) {
+                ans++;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
