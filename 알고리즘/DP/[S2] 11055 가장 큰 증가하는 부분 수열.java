031371ac7c9067b65bdde53a413be79a43f42a9c
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-09-18 23:22:39 +0900

    #11055

---
+++ 알고리즘/DP/[S2] 11055 가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] arr, dp;
+    static int MAX = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+        dp = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            dp[i] = arr[i];
+            for (int j = 1; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+
+        for (int max : dp) {
+            MAX = Math.max(max, MAX);
+        }
+
+        System.out.print(MAX);
+    }
+}
