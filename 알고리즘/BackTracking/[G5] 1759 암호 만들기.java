ca5a24b8e948795a74b8d6b40de422ba72488d2f
Jeong Hoon <52675944+JH627@users.noreply.github.com>
2025-03-26 11:16:44 +0900

    BOJ_1759_암호만들기

--- 알고리즘/BackTracking/[G5] 1759 암호 만들기.java
+++ 알고리즘/BackTracking/[G5] 1759 암호 만들기.java
@@ -1,56 +1,81 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 풀이
+ * 
+ * 1. 모든 암호 조합을 살펴보기
+ * 2. 만약 암호의 길이 제한에 도달 했다면 모음과 자음 개수가 주어진 조건에 부합하는지 확인
+ * 3. 만약 조건에 맞는다면 출력
+ *
+ */
+public class BOJ_1759_암호만들기 {
 
-    static int L, C;
-    static Character[] arr;
+	static BufferedReader br;
+	static StringBuilder sb;
+	 
+	static final char[] VOWELS = new char[]{'a', 'e', 'i', 'o', 'u'};
+	
+    static int selectCount, elementCount;
+    static Character[] characters;
     static boolean[] isVowel;
-    static StringBuilder sb = new StringBuilder();
-    static HashSet<Character> set = new HashSet<>();
-    static final Character[] vowels = new Character[]{'a', 'e', 'i', 'o', 'u'};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+    	init();
 
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-
-        arr = new Character[C];
-        st = new StringTokenizer(br.readLine());
-        for (int c = 0; c < C; c++) {
-            arr[c] = st.nextToken().charAt(0);
-        }
-
-        Arrays.sort(arr);
-        Collections.addAll(set, vowels);
-
-        isVowel = new boolean[C];
-        for (int c = 0; c < C; c++) {
-            isVowel[c] = set.contains(arr[c]);
-        }
-
-        bt(0, 0, 0, 0, "");
+        makePassword(0, 0, 0, 0, "");
 
         System.out.print(sb);
     }
 
-    static void bt(int idx, int depth, int vowels, int consonants, String s) {
-        if (depth == L) {
+    static void makePassword(int elementIndex, int selectedIndex, int vowels, int consonants, String s) {
+    	// 만약 문자를 다 골랐고
+        if (selectedIndex == selectCount) {
+        	// 조건에 맞다면 출력
             if (vowels >= 1 && consonants >= 2) {
                 sb.append(s).append("\n");
             }
             return;
         }
 
-        for (int i = idx; i <= C - L + depth; i++) {
-            bt(i + 1, depth + 1, (isVowel[i]) ? vowels + 1 : vowels, (isVowel[i]) ? consonants : consonants + 1, s + arr[i]);
+        // 암호를 완성할 수 있는 최대 인덱스까지 문자를 하나씩 가지고 분기
+        // 분기 시에는 모음인지 자음인지 카운팅하여 분기한다.
+        for (int index = elementIndex; index <= elementCount - selectCount + selectedIndex; index++) {
+        	makePassword(index + 1, selectedIndex + 1, 
+        			(isVowel[index]) ? vowels + 1 : vowels, 
+        			(isVowel[index]) ? consonants : consonants + 1, s + characters[index]);
+        }
+    }
+    
+    static void init() throws Exception {
+    	br = new BufferedReader(new InputStreamReader(System.in));
+    	sb = new StringBuilder();
+    	
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        selectCount = Integer.parseInt(st.nextToken());
+        elementCount = Integer.parseInt(st.nextToken());
+        
+        characters = new Character[elementCount];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int elementIndex = 0; elementIndex < elementCount; elementIndex++) {
+        	characters[elementIndex] = st.nextToken().charAt(0);
+        }
+
+        // 사전식으로 출력하기 위하여 정렬
+        Arrays.sort(characters);
+
+        // 모음인지 확인
+        isVowel = new boolean[elementCount];
+        for (int elementIndex = 0; elementIndex < elementCount; elementIndex++) {
+        	for (int index = 0; index < 5; index++) {
+        		if (characters[elementIndex] == VOWELS[index]) {
+        			isVowel[elementIndex] = true;
+        			break;
+        		}
+        	}
         }
     }
 }
